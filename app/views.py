fa67aaf21bac48bd9d082c851f244dd01d23642d
unknown <hibit_at@hotmail.com>
2021-06-19 20:47:17 +0900

    single var

--- app/views.py
+++ app/views.py
@@ -8,5 +8,15 @@ def index(request):
     data = Ranking.objects.order_by('date').reverse().all()
     for d in data:
         d.date = f'{d.date.day}'
+        d.n0 = d.n0 + '.jpg'
+        d.n1 = d.n1 + '.jpg'
+        d.n2 = d.n2 + '.jpg'
+        d.n3 = d.n3 + '.jpg'
+        d.n4 = d.n4 + '.jpg'
+        d.n5 = d.n5 + '.jpg'
+        d.n6 = d.n6 + '.jpg'
+        d.n7 = d.n7 + '.jpg'
+        d.n8 = d.n8 + '.jpg'
+        d.n9 = d.n9 + '.jpg'
     params = {'data' : data}
     return render(request, 'index.html', params)
