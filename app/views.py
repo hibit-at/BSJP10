e0b50a39dc81df3b939272dfa55d6433bc8b917d
unknown <hibit_at@hotmail.com>
2021-06-24 04:01:20 +0900

    pseudonavbar

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from django.db.models.query import QuerySet
+from itertools import chain
+
 from django.shortcuts import render
 
 from app.models import Ranking
@@ -7,6 +8,10 @@ from app.models import Ranking
 
 
 def index(request):
+    freq = request.GET.get('q')
+    if freq != 'day' and freq != 'month':
+        freq = 'week'
+    print(freq)
     years = [str(i) for i in range(2020,2022)]
     years.reverse()
     months = [str(i).zfill(2) for i in range(1,13)]
@@ -15,10 +20,20 @@ def index(request):
     for y in years:
         month_by_list = []
         for m in months:
-            data = Ranking.objects.filter(date__year=y).filter(date__month=m).order_by('date').reverse()
+            data = Ranking.objects.filter(date__year=y).filter(date__month=m)
+            if freq=='day':
+                data = data.order_by('date').reverse()
+            elif freq=='month':
+                data = data.filter(date__day = 1)
+            else:
+                data1 = data.filter(date__day = 1)
+                data2 = data.filter(date__day = 8)
+                data3 = data.filter(date__day = 15)
+                data4 = data.filter(date__day = 22)
+                data = list(chain(data4,data3,data2,data1))
             if len(data) == 0:
                 continue
             month_by_list.append({'month' : int(m) , 'data' : data})
         year_by_list.append({'year' : y, 'data' : month_by_list})
-    params = {'year_by' : year_by_list}
+    params = {'year_by' : year_by_list, 'freq' : freq}
     return render(request, 'index.html', params)
